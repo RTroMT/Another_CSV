8d334f55b61e428f3f399e8681e165bc22690944
RTroMT <n.minhtri1105@gmail.com>
2022-04-13 20:02:25 +0700

    Create files and find solutions

---
+++ csv_reader.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Input :
+// name.csv && numb_col && numb_row
+// Interface:
+// return value(x,y)
+// Open/Close lib.h (clean up mem)
